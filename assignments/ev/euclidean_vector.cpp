645fa3266d106778bfcacc7995ddf28a67d37bc3
matt <mattstark75@gmail.com>
2019-07-13 17:43:36 +1000

    cpplint

--- assignments/ev/euclidean_vector.cpp
+++ assignments/ev/euclidean_vector.cpp
@@ -1,96 +1,105 @@
 #include "assignments/ev/euclidean_vector.h"
 
+#include <algorithm>
 #include <cassert>
-#include <algorithm>  // Look at these - they are helpful https://en.cppreference.com/w/cpp/algorithm
+#include <utility>
 
-// MEMBER OVERLOADS ################################
+// MEMBER OVERLOADS
 
 // copy assignment
 EuclideanVector& EuclideanVector::operator=(const EuclideanVector& original) noexcept {
-        EuclideanVector copy{original};
-        std::swap(copy, *this);
-        return *this;
+  EuclideanVector copy{original};
+  std::swap(copy, *this);
+  return *this;
 }
 
 // move assignment (as given in specs)
 EuclideanVector& EuclideanVector::operator=(EuclideanVector&& original) noexcept {
-    magnitudes_ = std::move(original.magnitudes_);
-    dimensions_ = original.GetNumDimensions();
-    original.dimensions_ = 0;
-    return *this;
+  magnitudes_ = std::move(original.magnitudes_);
+  dimensions_ = original.GetNumDimensions();
+  original.dimensions_ = 0;
+  return *this;
 }
 
 // [] operator for reading
-double EuclideanVector::operator[](const int index) const noexcept{
-    assert(index < this->dimensions_ && index >= 0);
-    return this->magnitudes_[index];
+double EuclideanVector::operator[](const int index) const noexcept {
+  assert(index < this->dimensions_ && index >= 0);
+  return this->magnitudes_[index];
 }
 
 // [] operator for writing
-double& EuclideanVector::operator[](const int index) noexcept{
-    assert(index < this->dimensions_ && index >= 0);
-    return this->magnitudes_[index];
+double& EuclideanVector::operator[](const int index) noexcept {
+  assert(index < this->dimensions_ && index >= 0);
+  return this->magnitudes_[index];
 }
 
 // += operator
 EuclideanVector& EuclideanVector::operator+=(const EuclideanVector& e) {
-    if (e.dimensions_ != this->dimensions_) throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
-    for (auto i = 0; i < e.GetNumDimensions(); ++i) {
-        magnitudes_[i] = magnitudes_[i] + e[i];
-    }
-    return *this;
+  if (e.dimensions_ != this->dimensions_)
+    throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
+  for (auto i = 0; i < e.GetNumDimensions(); ++i) {
+    magnitudes_[i] = magnitudes_[i] + e[i];
+  }
+  return *this;
 }
 
 // -= operator
 EuclideanVector& EuclideanVector::operator-=(const EuclideanVector& e) {
-    if (e.dimensions_ != this->dimensions_) throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
-    for (auto i = 0; i < e.GetNumDimensions(); ++i) {
-        magnitudes_[i] = magnitudes_[i] - e[i];
-    }
-    return *this;
+  if (e.dimensions_ != this->dimensions_)
+    throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
+  for (auto i = 0; i < e.GetNumDimensions(); ++i) {
+    magnitudes_[i] = magnitudes_[i] - e[i];
+  }
+  return *this;
 }
 
 // *= operator
 EuclideanVector& EuclideanVector::operator*=(const int& n) noexcept {
-    for (auto i = 0; i < dimensions_; ++i) {
-        magnitudes_[i] = magnitudes_[i] * n;
-    }
-    return *this;
+  for (auto i = 0; i < dimensions_; ++i) {
+    magnitudes_[i] = magnitudes_[i] * n;
+  }
+  return *this;
 }
 
 // /= operator
-EuclideanVector& EuclideanVector::operator/=(const int& n){
-    if (n == 0) throw EuclideanVectorError("Invalid vector division by 0");
-    for (auto i = 0; i < dimensions_; ++i) {
-        magnitudes_[i] = magnitudes_[i] / n;
-    }
-    return *this;
+EuclideanVector& EuclideanVector::operator/=(const int& n) {
+  if (n == 0)
+    throw EuclideanVectorError("Invalid vector division by 0");
+  for (auto i = 0; i < dimensions_; ++i) {
+    magnitudes_[i] = magnitudes_[i] / n;
+  }
+  return *this;
 }
 
 // METHOD DEFINITIONS
 
 // Returns a Euclidean vector equal to the unit vector of the euclidean vector it was called from
 EuclideanVector EuclideanVector::CreateUnitVector() const {
-    // Exception handling
-    if (this->GetNumDimensions() == 0) throw EuclideanVectorError("EuclideanVector with no dimensions does not have a unit vector");
-    if (this->GetEuclideanNorm() == 0) throw  EuclideanVectorError("EuclideanVector with euclidean normal of 0 does not have a unit vector");
+  // Exception handling
+  if (this->GetNumDimensions() == 0)
+    throw EuclideanVectorError("EuclideanVector with no dimensions does not have a unit vector");
+  if (this->GetEuclideanNorm() == 0)
+    throw EuclideanVectorError(
+        "EuclideanVector with euclidean normal of 0 does not have a unit vector");
 
-    // getting the euclidean norm of the EV so we can calculate the values of the unit vector
-    double norm = this->GetEuclideanNorm();
-    // constructing an EV of the same size and filling it in with the correct magnitudes (the corresponding magnitudes divided by the norm)
-    EuclideanVector temp(dimensions_);
-    for (auto i = 0; i < dimensions_; ++i) {
-        temp[i] = this->magnitudes_[i]/norm;
-    }
-    return temp;
+  // getting the euclidean norm of the EV so we can calculate the values of the unit vector
+  double norm = this->GetEuclideanNorm();
+  // constructing an EV of the same size and filling it in with the correct magnitudes (the
+  // corresponding magnitudes divided by the norm)
+  EuclideanVector temp(dimensions_);
+  for (auto i = 0; i < dimensions_; ++i) {
+    temp[i] = this->magnitudes_[i] / norm;
+  }
+  return temp;
 }
 
 // calculates and returns the euclidean norm of an euclidean vector as a double
 double EuclideanVector::GetEuclideanNorm() const {
-    if (this->GetNumDimensions() == 0) throw EuclideanVectorError("EuclideanVector with no dimensions does not have a norm");
-    double sum = 0;
-    for (auto i = 0; i < dimensions_; ++i) {
-        sum = sum + pow(magnitudes_[i], 2);
-    }
-    return std::sqrt(sum);
+  if (this->GetNumDimensions() == 0)
+    throw EuclideanVectorError("EuclideanVector with no dimensions does not have a norm");
+  double sum = 0;
+  for (auto i = 0; i < dimensions_; ++i) {
+    sum = sum + pow(magnitudes_[i], 2);
+  }
+  return std::sqrt(sum);
 }
--- assignments/ev/euclidean_vector.h
+++ assignments/ev/euclidean_vector.h
@@ -1,223 +1,241 @@
-#ifndef EUCLIDEAN_VECTOR_H
-#define EUCLIDEAN_VECTOR_H
+#ifndef ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
+#define ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
 
-#include <exception>
-#include <string>
-#include <memory>
-#include <iostream>
-#include <vector>
 #include <cmath>
+#include <exception>
+#include <iostream>
 #include <iterator>
 #include <list>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
 class EuclideanVectorError : public std::exception {
  public:
   explicit EuclideanVectorError(const std::string& what) : what_(what) {}
-  const char* what() const noexcept{ return what_.c_str(); }
+  const char* what() const noexcept { return what_.c_str(); }
+
  private:
   std::string what_;
 };
 
 class EuclideanVector {
  public:
-    // CONSTRUCTORS
+  // CONSTRUCTORS
 
-    // default constructor
-    explicit EuclideanVector(int dimensions = 1) : EuclideanVector{dimensions, 0} {
+  // default constructor
+  explicit EuclideanVector(int dimensions = 1) : EuclideanVector{dimensions, 0} {}
+
+  // regular constructor
+  EuclideanVector(int dimensions, double magnitudes)
+    : dimensions_{dimensions}, magnitudes_{std::make_unique<double[]>(dimensions)} {
+    for (auto j = 0; j < dimensions; ++j) {
+      magnitudes_[j] = magnitudes;
     }
+  }
 
-    // regular constructor
-    EuclideanVector(int dimensions, double magnitudes) : dimensions_{dimensions}, magnitudes_{std::make_unique<double[]>(dimensions)} {
-        for (auto j = 0; j < dimensions; ++j) {
-            magnitudes_[j] = magnitudes;
-        }
+  // iterator constructor
+  EuclideanVector(std::vector<double>::const_iterator begin,
+                  std::vector<double>::const_iterator end) {
+    int n_dimensions = 0;
+    // counting the number of dimensions in the vector
+    for (auto iter = begin; iter != end; ++iter) {
+      n_dimensions++;
     }
-
-    // iterator constructor
-    EuclideanVector(std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end) {
-        int n_dimensions = 0;
-        // counting the number of dimensions in the vector
-        for (auto iter = begin; iter != end; ++iter) {
-            n_dimensions++;
-        }
-        dimensions_ = n_dimensions;
-        magnitudes_ = std::make_unique<double[]>(n_dimensions);
-        auto index = 0;
-        // copying the value of each element in the vector into the newly constructed EV
-        for (auto iter = begin; iter != end; ++iter) {
-            magnitudes_[index] = *iter;
-            ++index;
-        }
+    dimensions_ = n_dimensions;
+    magnitudes_ = std::make_unique<double[]>(n_dimensions);
+    auto index = 0;
+    // copying the value of each element in the vector into the newly constructed EV
+    for (auto iter = begin; iter != end; ++iter) {
+      magnitudes_[index] = *iter;
+      ++index;
     }
+  }
 
-    // copy constructor
-    EuclideanVector(const EuclideanVector& original) : dimensions_(original.dimensions_) {
-        magnitudes_ = std::make_unique<double[]>(original.GetNumDimensions());
-        for (int i = 0; i < original.GetNumDimensions(); ++i) {
-            magnitudes_[i] = original[i];
-        }
+  // copy constructor
+  EuclideanVector(const EuclideanVector& original) : dimensions_(original.dimensions_) {
+    magnitudes_ = std::make_unique<double[]>(original.GetNumDimensions());
+    for (int i = 0; i < original.GetNumDimensions(); ++i) {
+      magnitudes_[i] = original[i];
     }
+  }
 
-    // move constructor (as given in specs)
-    EuclideanVector(EuclideanVector&& o) noexcept : dimensions_{o.dimensions_}, magnitudes_{std::move(o.magnitudes_)} {
-        o.dimensions_ = 0;
+  // move constructor (as given in specs)
+  EuclideanVector(EuclideanVector&& o) noexcept
+    : dimensions_{o.dimensions_}, magnitudes_{std::move(o.magnitudes_)} {
+    o.dimensions_ = 0;
+  }
+
+  // default destructor
+  ~EuclideanVector() noexcept = default;
+
+  // MEMBER FUNCTIONS
+
+  // copy operator (used to copy one EV to another)
+  EuclideanVector& operator=(const EuclideanVector& original) noexcept;
+  // move operator (used to move everything from one EV to another, so the original becomes empty)
+  EuclideanVector& operator=(EuclideanVector&& original) noexcept;
+  // += operator
+  EuclideanVector& operator+=(const EuclideanVector& e);
+  // -= operator
+  EuclideanVector& operator-=(const EuclideanVector& e);
+  // *= operator
+  EuclideanVector& operator*=(const int& n) noexcept;
+  // /= operator
+  EuclideanVector& operator/=(const int& n);
+  // [] operator for writing/setting values
+  double& operator[](int index) noexcept;
+  // [] operator for reading values
+  double operator[](int index) const noexcept;
+
+  // Vector type conversion
+  explicit operator std::vector<double>() const noexcept {
+    std::vector<double> temp;
+    for (auto i = 0; i < this->dimensions_; ++i) {
+      temp.emplace_back(this->magnitudes_[i]);
     }
+    return temp;
+  }
 
-    // default destructor
-    ~EuclideanVector() noexcept = default;
-
-    // MEMBER FUNCTIONS
-
-    // copy operator (used to copy one EV to another)
-    EuclideanVector& operator=(const EuclideanVector& original) noexcept;
-    // move operator (used to move everything from one EV to another, so the original becomes empty)
-    EuclideanVector& operator=(EuclideanVector&& original) noexcept;
-    // += operator
-    EuclideanVector& operator+=(const EuclideanVector& e);
-    // -= operator
-    EuclideanVector& operator-=(const EuclideanVector& e);
-    // *= operator
-    EuclideanVector& operator*=(const int& n) noexcept;
-    // /= operator
-    EuclideanVector& operator/=(const int& n);
-    // [] operator for writing/setting values
-    double& operator[](int index) noexcept;
-    // [] operator for reading values
-    double operator[](int index) const noexcept;
-
-    // Vector type conversion
-    explicit operator std::vector<double>() const noexcept {
-        std::vector<double> temp;
-        for (auto i = 0; i < this->dimensions_; ++i) {
-            temp.emplace_back(this->magnitudes_[i]);
-        }
-        return temp;
+  // List type conversion
+  explicit operator std::list<double>() const noexcept {
+    std::list<double> temp;
+    for (auto i = 0; i < this->dimensions_; ++i) {
+      temp.emplace_back(this->magnitudes_[i]);
     }
+    return temp;
+  }
 
-    // List type conversion
-    explicit operator std::list<double>() const noexcept {
-        std::list<double> temp;
-        for (auto i = 0; i < this->dimensions_; ++i) {
-            temp.emplace_back(this->magnitudes_[i]);
-        }
-        return temp;
+  // FRIENDS
+
+  // == operator to check if two EVs are identical. Returns true if they are, false otherwise
+  friend bool operator==(const EuclideanVector& v1, const EuclideanVector& v2) noexcept {
+    if (v1.dimensions_ == v2.dimensions_) {
+      for (auto i = 0; i < v1.dimensions_; ++i) {
+        if (v1[i] != v2[i])
+          return false;
+      }
+      return true;
     }
+    return false;
+  }
 
-    // FRIENDS
+  // == operator to check if two EVs are different. returns true if they are, false otherwise
+  friend bool operator!=(const EuclideanVector& v1, const EuclideanVector& v2) noexcept {
+    if (v1 == v2)
+      return false;
+    return true;
+  }
 
-    // == operator to check if two EVs are identical. Returns true if they are, false otherwise
-    friend bool operator==(const EuclideanVector& v1, const EuclideanVector& v2) noexcept {
-        if (v1.dimensions_ == v2.dimensions_) {
-            for (auto i = 0; i < v1.dimensions_; ++i) {
-                if (v1[i] != v2[i]) return false;
-            }
-            return true;
-        }
-        return false;
+  // + operator to add two EVs. Throws exception if the two EVs have different dimensions
+  friend EuclideanVector operator+(const EuclideanVector& v1, const EuclideanVector& v2) {
+    if (v1.GetNumDimensions() != v2.GetNumDimensions())
+      throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
+    EuclideanVector sum = EuclideanVector{v1.dimensions_};
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      sum[i] = v1[i] + v2[i];
     }
+    return sum;
+  }
 
-    // == operator to check if two EVs are different. returns true if they are, false otherwise
-    friend bool operator!=(const EuclideanVector& v1, const EuclideanVector& v2) noexcept {
-        if (v1 == v2) return false;
-        return true;
+  // - operator to subtract 2 EVs. Throws exception if the two EVs have different dimensions
+  friend EuclideanVector operator-(const EuclideanVector& v1, const EuclideanVector& v2) {
+    if (v1.GetNumDimensions() != v2.GetNumDimensions())
+      throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
+    EuclideanVector v = EuclideanVector{v1.dimensions_};
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      v[i] = v1[i] - v2[i];
     }
+    return v;
+  }
 
-    // + operator to add two EVs. Throws exception if the two EVs have different dimensions
-    friend EuclideanVector operator+(const EuclideanVector& v1, const EuclideanVector& v2) {
-        if (v1.GetNumDimensions() != v2.GetNumDimensions()) throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
-        EuclideanVector sum = EuclideanVector{v1.dimensions_};
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            sum[i] = v1[i] + v2[i];
-        }
-        return sum;
+  // * operator to find the dot product of 2 EVs. Throws exception if the two EVs have different
+  // dimensions
+  friend double operator*(const EuclideanVector& v1, const EuclideanVector& v2) {
+    if (v1.GetNumDimensions() != v2.GetNumDimensions())
+      throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
+    double dot_product = 0;
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      dot_product = dot_product + (v2[i] * v1[i]);
     }
+    return dot_product;
+  }
 
-    // - operator to subtract 2 EVs. Throws exception if the two EVs have different dimensions
-    friend EuclideanVector operator-(const EuclideanVector& v1, const EuclideanVector& v2) {
-        if (v1.GetNumDimensions() != v2.GetNumDimensions()) throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
-        EuclideanVector v = EuclideanVector{v1.dimensions_};
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            v[i] = v1[i] - v2[i];
-        }
-        return v;
+  // * operator to multiply an EV by a scalar, where the scalar comes after the *
+  friend EuclideanVector operator*(const EuclideanVector& v1, const int& n) noexcept {
+    EuclideanVector v = EuclideanVector{v1.dimensions_};
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      v[i] = (v1[i] * n);
     }
+    return v;
+  }
 
-    // * operator to find the dot product of 2 EVs. Throws exception if the two EVs have different dimensions
-    friend double operator*(const EuclideanVector& v1, const EuclideanVector& v2) {
-        if (v1.GetNumDimensions() != v2.GetNumDimensions()) throw EuclideanVectorError("Dimensions of LHS(X) and RHS(Y) do not match");
-        double dot_product = 0;
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            dot_product = dot_product + (v2[i] * v1[i]);
-        }
-        return dot_product;
+  // * operator to multiply an EV by a scalar, where the scalar comes before the *
+  friend EuclideanVector operator*(const int& n, const EuclideanVector& v1) noexcept {
+    EuclideanVector v = EuclideanVector{v1.dimensions_};
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      v[i] = (v1[i] * n);
     }
+    return v;
+  }
 
-    // * operator to multiply an EV by a scalar, where the scalar comes after the *
-    friend EuclideanVector operator*(const EuclideanVector& v1, const int& n) noexcept {
-        EuclideanVector v = EuclideanVector{v1.dimensions_};
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            v[i] = (v1[i] * n);
-        }
-        return v;
+  // division operator to divide each dimension of an EV by a scalar. Throws exception if trying to
+  // divide by 0
+  friend EuclideanVector operator/(const EuclideanVector& v1, const int& n) {
+    if (n == 0)
+      throw EuclideanVectorError("Invalid vector division by 0");
+    EuclideanVector v = EuclideanVector{v1.dimensions_};
+    for (auto i = 0; i < v1.dimensions_; ++i) {
+      v[i] = (v1[i] / n);
     }
+    return v;
+  }
 
-    // * operator to multiply an EV by a scalar, where the scalar comes before the *
-    friend EuclideanVector operator*(const int& n, const EuclideanVector& v1) noexcept {
-        EuclideanVector v = EuclideanVector{v1.dimensions_};
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            v[i] = (v1[i] * n);
-        }
-        return v;
+  // output stream operator to print out the contents of an EV in the form [1,2,3]
+  friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v) noexcept {
+    os << "[";
+    for (auto i = 0; i < v.dimensions_; ++i) {
+      os << v[i];
+      if (i != (v.dimensions_ - 1))
+        os << " ";
     }
+    os << "]";
+    return os;
+  }
 
-    // division operator to divide each dimension of an EV by a scalar. Throws exception if trying to divide by 0
-    friend EuclideanVector operator/(const EuclideanVector& v1, const int& n) {
-        if (n == 0) throw EuclideanVectorError("Invalid vector division by 0");
-        EuclideanVector v = EuclideanVector{v1.dimensions_};
-        for (auto i = 0; i < v1.dimensions_; ++i) {
-            v[i] = (v1[i] / n);
-        }
-        return v;
-    }
+  // METHODS
 
-    // output stream operator to print out the contents of an EV in the form [1,2,3]
-    friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v) noexcept {
-        os << "[";
-        for (auto i = 0; i < v.dimensions_; ++i) {
-            os << v[i];
-            if (i != (v.dimensions_-1)) os << " ";
-        }
-        os <<  "]";
-        return os;
-    }
+  // at method to get the value at a certain index in an EV. Throws exception if the index is out of
+  // bounds
+  double at(const int& n) const {
+    if (n < 0 || n >= dimensions_)
+      throw EuclideanVectorError("Index X is not valid for this EuclideanVector object");
+    return magnitudes_[n];
+  }
 
-    // METHODS
+  // at method to set the value at a certain index in an EV. Throws exception if the index is out of
+  // bounds
+  double& at(const int& n) {
+    if (n < 0 || n >= dimensions_)
+      throw EuclideanVectorError("Index X is not valid for this EuclideanVector object");
+    return magnitudes_[n];
+  }
 
-    // at method to get the value at a certain index in an EV. Throws exception if the index is out of bounds
-    double at(const int& n) const {
-        if (n < 0 || n >= dimensions_) throw EuclideanVectorError("Index X is not valid for this EuclideanVector object");
-        return magnitudes_[n];
-    }
+  // method to get the number of dimensions in an EV
+  int GetNumDimensions() const noexcept { return dimensions_; }
 
-    // at method to set the value at a certain index in an EV. Throws exception if the index is out of bounds
-    double& at(const int& n) {
-        if (n < 0 || n >= dimensions_) throw EuclideanVectorError("Index X is not valid for this EuclideanVector object");
-        return magnitudes_[n];
-    }
+  // method to get the Euclidean Norm of an EV. Throws exception if the number of dimensions in the
+  // EV is 0
+  double GetEuclideanNorm() const;
 
-    // method to get the number of dimensions in an EV
-    int GetNumDimensions() const noexcept { return dimensions_;}
-
-    // method to get the Euclidean Norm of an EV. Throws exception if the number of dimensions in the EV is 0
-    double GetEuclideanNorm() const;
-
-    // method to create a unit vector from an EV. Throws exception if the EV has no dimensions or has a euclidean normal of 0
-    EuclideanVector CreateUnitVector() const;
+  // method to create a unit vector from an EV. Throws exception if the EV has no dimensions or has
+  // a euclidean normal of 0
+  EuclideanVector CreateUnitVector() const;
 
  private:
-    int dimensions_; // stores number of dimensions in the EV
-    std::unique_ptr<double[]> magnitudes_;
+  int dimensions_;  // stores number of dimensions in the EV
+  std::unique_ptr<double[]> magnitudes_;
 };
 
-#endif
-
+#endif  // ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
